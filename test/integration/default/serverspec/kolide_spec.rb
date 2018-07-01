71e3264d1a25081f87f187742e015e868d39eb2d
juju4 <juju4@users.noreply.github.com>
2018-07-01 12:43:31 -0400

    move to latest fleet - archive install

--- test/integration/default/serverspec/kolide_spec.rb
+++ test/integration/default/serverspec/kolide_spec.rb
@@ -14,7 +14,7 @@ describe service('kolide') do
   it { should be_enabled }
   it { should be_running }
 end  
-describe process("kolide") do
+describe process("fleet") do
   its(:user) { should eq "_kolide" }
   its(:args) { should match /--config \/etc\/kolide\/kolide.yml/ }
   its(:count) { should eq 1 }
@@ -26,14 +26,14 @@ describe port(8080) do
 end
 
 describe file('/var/log/syslog'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
-  its(:content) { should match /kolide\[[0-9]+\]: Using config file:/ }
-  its(:content) { should match /kolide\[[0-9]+\]: {"component":"license-checker",/ }
-  its(:content) { should match /kolide\[[0-9]+\]: {"address":"0.0.0.0:8080","msg":"listening","transport":/ }
-  its(:content) { should_not match /kolide\[[0-9]+\]: .*permission denied.*/ }
+  its(:content) { should match /fleet\[[0-9]+\]: Using config file:/ }
+  its(:content) { should match /fleet\[[0-9]+\]: {"component":"license-checker",/ }
+  its(:content) { should match /fleet\[[0-9]+\]: {"address":"0.0.0.0:8080","msg":"listening","transport":/ }
+  its(:content) { should_not match /fleet\[[0-9]+\]: .*permission denied.*/ }
 end
 describe file('/var/log/messages'), :if => os[:family] == 'redhat' do
-  its(:content) { should match /kolide: Using config file:/ }
-  its(:content) { should match /kolide: {"component":"license-checker",/ }
-  its(:content) { should match /kolide: {"address":"0.0.0.0:8080","msg":"listening","transport":/ }
-  its(:content) { should_not match /kolide: .*permission denied.*/ }
+  its(:content) { should match /fleet: Using config file:/ }
+  its(:content) { should match /fleet: {"component":"license-checker",/ }
+  its(:content) { should match /fleet: {"address":"0.0.0.0:8080","msg":"listening","transport":/ }
+  its(:content) { should_not match /fleet: .*permission denied.*/ }
 end
