46fecd0c325dc8c17020c7952629da09dbcbfb33
juju4 <juju4@users.noreply.github.com>
2018-12-16 14:24:17 -0500

    galaxy lint + dependency naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/geerlingguy.redis ] && git clone https://github.com/juju4/ansible-role-redis.git $rolesdir/geerlingguy.redis
 [ ! -d $rolesdir/geerlingguy.mysql ] && git clone https://github.com/geerlingguy/ansible-role-mysql.git $rolesdir/geerlingguy.mysql
 ## galaxy naming: kitchen fails to transfer symlink folder
@@ -18,4 +18,3 @@ fi
 
 ## don't stop build on this script return code
 true
-
